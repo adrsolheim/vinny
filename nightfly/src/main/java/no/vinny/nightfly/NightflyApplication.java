86c66431dce3809b7dee3cd3364dcba08698ea07
adrsolheim <adrsolheim@gmail.com>
2023-04-23 17:26:19 +0200

    initalize project

---
+++ nightfly/src/main/java/no/vinny/nightfly/NightflyApplication.java
@@ -0,0 +1,13 @@
+package no.vinny.nightfly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NightflyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NightflyApplication.class, args);
+	}
+
+}
