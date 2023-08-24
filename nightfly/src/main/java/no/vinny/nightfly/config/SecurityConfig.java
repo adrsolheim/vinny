959b18739adb9d0308187a0ce82ebd09c2bff22d
Adrian Solheim <adrsolheim@gmail.com>
2023-08-24 20:20:12 +0200

    Set response message on auth failure by intercepting auth exception

--- nightfly/src/main/java/no/vinny/nightfly/config/SecurityConfig.java
+++ nightfly/src/main/java/no/vinny/nightfly/config/SecurityConfig.java
@@ -1,40 +1,40 @@
 package no.vinny.nightfly.config;
 
+import no.vinny.nightfly.util.RequestExceptionInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
-
-// TODO: add dev profile to new noSecurity config class
-//@Profile("default")
 @EnableWebFluxSecurity
 //@EnableMethodSecurity TODO: preauth intrusive requests
 @Configuration
 public class SecurityConfig {
+
+    private final RequestExceptionInterceptor interceptor;
+
+    @Autowired
+    public SecurityConfig(RequestExceptionInterceptor interceptor) {
+        this.interceptor = interceptor;
+    }
+
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
         return http
+                .exceptionHandling()
+                .authenticationEntryPoint(interceptor)
+                .and()
+                // TODO: enable csrf
                 .csrf().disable()
-                //.cors().disable()
-                //.authorizeExchange()
                 .authorizeExchange()
                 .pathMatchers(HttpMethod.GET, "/api/**").permitAll()
                 .pathMatchers(HttpMethod.POST, "/api/batches").permitAll()
                 .pathMatchers(HttpMethod.PUT, "/api/batches/**").permitAll()
                 .pathMatchers(HttpMethod.PATCH, "/api/batches/**").permitAll()
                 .and()
-                //.pathMatchers(HttpMethod.POST, "/api/**").permitAll()
-                //.anyExchange().permitAll()
-                //.and()
-                //.httpBasic()
-                //.and()
                 .build();
     }
 }
