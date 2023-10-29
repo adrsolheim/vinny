591d1b200088eaf8c7b926dab43025bcd489341f
Adrian Solheim <adrsolheim@gmail.com>
2023-10-29 20:39:20 +0100

    Add foreign key constraint: Batch -> Recipe

--- nightfly/src/main/resources/h2init.sql
+++ nightfly/src/main/resources/h2init.sql
@@ -1,14 +1,7 @@
 --DROP DATABASE IF EXISTS nightfly;
 --CREATE DATABASE IF NOT EXISTS nightfly;
-DROP TABLE IF EXISTS batch;
 DROP TABLE IF EXISTS recipe;
-
-CREATE TABLE IF NOT EXISTS batch (
-    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    brewfather_id VARCHAR(50) UNIQUE,
-    name VARCHAR(100),
-    status VARCHAR(30)
-    );
+DROP TABLE IF EXISTS batch;
 
 CREATE TABLE IF NOT EXISTS recipe (
     id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -16,12 +9,21 @@ CREATE TABLE IF NOT EXISTS recipe (
     name VARCHAR(100)
     );
 
-INSERT INTO batch (brewfather_id, name, status) VALUES ('HY27A73dYWZNMxapgE4UdljPtNvDOL', 'MarisOtter SMASH', 'COMPLETED');
-INSERT INTO batch (brewfather_id, name, status) VALUES ('LAXI2KWZXcU2pBpzrfg6B3Uy5940vQ', 'Eldon',            'COMPLETED');
-INSERT INTO batch (brewfather_id, name, status) VALUES ('NLltIcoo87foHbTz1N8rH0v0KXht6q', 'Juicy SafAle',     'COMPLETED');
-INSERT INTO batch (brewfather_id, name, status) VALUES ('LAXI2KWZXcU2pBpzrfg6B3Uy5940vS', 'Kiwi',             'COMPLETED');
-INSERT INTO batch (brewfather_id, name, status) VALUES ('NLltIcoo87foHbTz1N8rH0v0KXht6P', 'Juicy NEIPA',      'COMPLETED');
-INSERT INTO batch (brewfather_id, name, status) VALUES ('NLltIcoo87foHbTz1N8rH0v0KXht6A', 'NETTO',            'COMPLETED');
+CREATE TABLE IF NOT EXISTS batch (
+    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    brewfather_id VARCHAR(50) UNIQUE,
+    name VARCHAR(100),
+    status VARCHAR(30),
+    recipe BIGINT,
+    FOREIGN KEY (recipe) REFERENCES recipe(id)
+    );
 
-INSERT INTO recipe (brewfather_id, name) VALUES ('b1W72a2OuL9xsC1c1OTrgog6JiPP0b', 'Eldon');
-INSERT INTO recipe (brewfather_id, name) VALUES ('EuWQsWWa3Q6SNG6lSx1xCuIDL3gAwz', 'Juicy Django (Kveik)');
+INSERT INTO recipe (id, brewfather_id, name) VALUES (100001, 'b1W72a2OuL9xsC1c1OTrgog6JiPP0b', 'Eldon');
+INSERT INTO recipe (id, brewfather_id, name) VALUES (100002, 'oCJlXhR1iIdYgLMwem0V0mmYNBIIia', 'KIWI Pilsner');
+
+INSERT INTO batch (brewfather_id, name, status, recipe) VALUES ('LAXI2KWZXcU2pBpzrfg6B3Uy5940vQ', 'Eldon',        'COMPLETED', 100001);
+INSERT INTO batch (brewfather_id, name, status, recipe) VALUES ('6TY4Hg2JfQA0zBnscwspPsVF29JAKM', 'KIWI Pilsner', 'COMPLETED', 100002);
+INSERT INTO batch (brewfather_id, name, status) VALUES ('HY27A73dYWZNMxapgE4UdljPtNvDOL', 'MarisOtter SMASH',     'COMPLETED');
+INSERT INTO batch (brewfather_id, name, status) VALUES ('NLltIcoo87foHbTz1N8rH0v0KXht6q', 'Juicy SafAle',         'COMPLETED');
+INSERT INTO batch (brewfather_id, name, status) VALUES ('NLltIcoo87foHbTz1N8rH0v0KXht6P', 'Juicy NEIPA',          'COMPLETED');
+INSERT INTO batch (brewfather_id, name, status) VALUES ('NLltIcoo87foHbTz1N8rH0v0KXht6A', 'NETTO',                'COMPLETED');
--- nightfly_db/scripts/1_init.sql
+++ nightfly_db/scripts/1_init.sql
@@ -1,15 +1,17 @@
 DROP DATABASE IF EXISTS nightfly;
 CREATE DATABASE IF NOT EXISTS nightfly;
 
-CREATE TABLE nightfly.batch (
-    id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    brewfather_id VARCHAR(50) UNIQUE,
-    name VARCHAR(100),
-    status VARCHAR(30)
-    );
-
 CREATE TABLE nightfly.recipe (
     id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
     brewfather_id VARCHAR(50) UNIQUE,
     name VARCHAR(100)
+    );
+
+CREATE TABLE nightfly.batch (
+    id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    brewfather_id VARCHAR(50) UNIQUE,
+    name VARCHAR(100),
+    status VARCHAR(30),
+    recipe BIGINT,
+    FOREIGN KEY (recipe) REFERENCES nightfly.recipe(id)
     );
