36d09724cdb097dd1354c2bf69e0d5c81b8e0ffd
Adrian Solheim <adrsolheim@gmail.com>
2023-10-30 20:21:29 +0100

    🔒 Cors default and disable csrf

--- nightfly/src/main/java/no/vinny/nightfly/config/SecurityConfig.java
+++ nightfly/src/main/java/no/vinny/nightfly/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package no.vinny.nightfly.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -13,13 +14,16 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http.authorizeHttpRequests((request) -> { request
-                .requestMatchers(HttpMethod.POST, "/api/batches").authenticated()
-                .requestMatchers(HttpMethod.PUT, "/api/batches/**").authenticated()
-                .requestMatchers(HttpMethod.PATCH, "/api/batches/**").authenticated()
-                .requestMatchers(HttpMethod.DELETE, "/api/batches/**").authenticated()
-                .requestMatchers(HttpMethod.GET, "/api/**").permitAll()
-                .anyRequest().permitAll();
+        return http
+                .csrf((csrf) -> csrf.disable())
+                .cors(Customizer.withDefaults())
+                .authorizeHttpRequests((request) -> { request
+                    .anyRequest().permitAll();
+                    //.requestMatchers(HttpMethod.POST, "/api/batches").authenticated()
+                    //.requestMatchers(HttpMethod.PUT, "/api/batches/**").authenticated()
+                    //.requestMatchers(HttpMethod.PATCH, "/api/batches/**").authenticated()
+                    //.requestMatchers(HttpMethod.DELETE, "/api/batches/**").authenticated()
+                    //.requestMatchers(HttpMethod.GET, "/api/**").permitAll()
         }).build();
     }
 }
