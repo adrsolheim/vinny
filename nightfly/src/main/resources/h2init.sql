2dee1d395289be74ac9f0748a5f91a89694f0098
Adrian Solheim <adrsolheim@gmail.com>
2023-11-17 21:09:24 +0100

    Update database with TapStatus

--- nightfly/src/main/resources/h2init.sql
+++ nightfly/src/main/resources/h2init.sql
@@ -14,6 +14,7 @@ CREATE TABLE IF NOT EXISTS batch (
     brewfather_id VARCHAR(50) UNIQUE,
     name VARCHAR(100),
     status VARCHAR(30),
+    tap_status VARCHAR(30),
     packaging VARCHAR(20),
     recipe BIGINT,
     FOREIGN KEY (recipe) REFERENCES recipe(id)
@@ -22,9 +23,9 @@ CREATE TABLE IF NOT EXISTS batch (
 INSERT INTO recipe (id, brewfather_id, name) VALUES (100001, 'b1W72a2OuL9xsC1c1OTrgog6JiPP0b', 'Eldon');
 INSERT INTO recipe (id, brewfather_id, name) VALUES (100002, 'oCJlXhR1iIdYgLMwem0V0mmYNBIIia', 'KIWI Pilsner');
 
-INSERT INTO batch (brewfather_id, name, status, packaging, recipe) VALUES ('LAXI2KWZXcU2pBpzrfg6B3Uy5940vQ', 'Eldon',            'COMPLETED', 'KEG', 100001);
-INSERT INTO batch (brewfather_id, name, status, packaging, recipe) VALUES ('6TY4Hg2JfQA0zBnscwspPsVF29JAKM', 'KIWI Pilsner',     'COMPLETED', 'KEG', 100002);
-INSERT INTO batch (brewfather_id, name, status, packaging)         VALUES ('HY27A73dYWZNMxapgE4UdljPtNvDOL', 'MarisOtter SMASH', 'COMPLETED', 'KEG');
-INSERT INTO batch (brewfather_id, name, status, packaging)         VALUES ('NLltIcoo87foHbTz1N8rH0v0KXht6q', 'Juicy SafAle',     'COMPLETED', 'KEG');
-INSERT INTO batch (brewfather_id, name, status, packaging)         VALUES ('NLltIcoo87foHbTz1N8rH0v0KXht6P', 'Juicy NEIPA',      'COMPLETED', 'KEG');
-INSERT INTO batch (brewfather_id, name, status, packaging)         VALUES ('NLltIcoo87foHbTz1N8rH0v0KXht6A', 'NETTO',            'COMPLETED', 'KEG');
+INSERT INTO batch (brewfather_id, name, status, tap_status, packaging, recipe) VALUES ('LAXI2KWZXcU2pBpzrfg6B3Uy5940vQ', 'Eldon',            'COMPLETED', 'WAITING', 'KEG', 100001);
+INSERT INTO batch (brewfather_id, name, status, tap_status, packaging, recipe) VALUES ('6TY4Hg2JfQA0zBnscwspPsVF29JAKM', 'KIWI Pilsner',     'COMPLETED', 'WAITING', 'KEG', 100002);
+INSERT INTO batch (brewfather_id, name, status, tap_status, packaging)         VALUES ('HY27A73dYWZNMxapgE4UdljPtNvDOL', 'MarisOtter SMASH', 'COMPLETED', 'WAITING', 'KEG');
+INSERT INTO batch (brewfather_id, name, status, tap_status, packaging)         VALUES ('NLltIcoo87foHbTz1N8rH0v0KXht6q', 'Juicy SafAle',     'COMPLETED', 'WAITING', 'KEG');
+INSERT INTO batch (brewfather_id, name, status, tap_status, packaging)         VALUES ('NLltIcoo87foHbTz1N8rH0v0KXht6P', 'Juicy NEIPA',      'COMPLETED', 'WAITING', 'KEG');
+INSERT INTO batch (brewfather_id, name, status, tap_status, packaging)         VALUES ('NLltIcoo87foHbTz1N8rH0v0KXht6A', 'NETTO',            'COMPLETED', 'WAITING', 'KEG');
