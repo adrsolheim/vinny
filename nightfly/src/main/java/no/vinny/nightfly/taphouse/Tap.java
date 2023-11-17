c4f980916758caa660a1c044ca44e0435a5c1e45
Adrian Solheim <adrsolheim@gmail.com>
2023-11-17 21:21:05 +0100

    Introducing taps

---
+++ nightfly/src/main/java/no/vinny/nightfly/taphouse/Tap.java
@@ -0,0 +1,11 @@
+package no.vinny.nightfly.taphouse;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Data
+public class Tap {
+    private final String name;
+
+}
