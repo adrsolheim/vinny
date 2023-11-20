f7534d500b3e841afebfb50bebb4efe0bc881d03
Adrian Solheim <adrsolheim@gmail.com>
2023-11-20 19:56:29 +0100

    Tap references Batch over BatchDTO

--- nightfly/src/main/java/no/vinny/nightfly/taphouse/Tap.java
+++ nightfly/src/main/java/no/vinny/nightfly/taphouse/Tap.java
@@ -2,12 +2,14 @@ package no.vinny.nightfly.taphouse;
 
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import no.vinny.nightfly.batch.domain.Batch;
+import no.vinny.nightfly.batch.domain.BatchDTO;
 
 @RequiredArgsConstructor
 @Data
 public class Tap implements Comparable<Tap> {
     private final Long id;
-    private final String name;
+    private final Batch batch;
 
     @Override
     public int compareTo(Tap other) {
--- nightfly/src/main/resources/h2init.sql
+++ nightfly/src/main/resources/h2init.sql
@@ -1,14 +1,8 @@
 --DROP DATABASE IF EXISTS nightfly;
 --CREATE DATABASE IF NOT EXISTS nightfly;
+DROP TABLE IF EXISTS tap;
 DROP TABLE IF EXISTS batch;
 DROP TABLE IF EXISTS recipe;
-DROP TABLE IF EXISTS tap;
-
-CREATE TABLE IF NOT EXISTS tap (
-    id TINYINT NOT NULL PRIMARY KEY,
-    batch BIGINT,
-    FOREIGN KEY batch REFERENCES batch(id)
-);
 
 CREATE TABLE IF NOT EXISTS recipe (
     id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -28,17 +22,24 @@ CREATE TABLE IF NOT EXISTS batch (
     FOREIGN KEY (recipe) REFERENCES recipe(id)
 );
 
-INSERT INTO tap (id) VALUES (1);
-INSERT INTO tap (id) VALUES (2);
-INSERT INTO tap (id) VALUES (3);
-INSERT INTO tap (id) VALUES (4);
+CREATE TABLE IF NOT EXISTS tap (
+    id TINYINT NOT NULL PRIMARY KEY,
+    batch BIGINT,
+    FOREIGN KEY (batch) REFERENCES batch(id)
+);
 
 INSERT INTO recipe (id, brewfather_id, name) VALUES (100001, 'b1W72a2OuL9xsC1c1OTrgog6JiPP0b', 'Eldon');
 INSERT INTO recipe (id, brewfather_id, name) VALUES (100002, 'oCJlXhR1iIdYgLMwem0V0mmYNBIIia', 'KIWI Pilsner');
 
-INSERT INTO batch (brewfather_id, name, status, tap_status, packaging, recipe, tap) VALUES ('LAXI2KWZXcU2pBpzrfg6B3Uy5940vQ', 'Eldon',            'COMPLETED', 'SERVING', 'KEG', 100001, 1);
-INSERT INTO batch (brewfather_id, name, status, tap_status, packaging, recipe, tap) VALUES ('6TY4Hg2JfQA0zBnscwspPsVF29JAKM', 'KIWI Pilsner',     'COMPLETED', 'SERVING', 'KEG', 100002, 2);
-INSERT INTO batch (brewfather_id, name, status, tap_status, packaging)              VALUES ('HY27A73dYWZNMxapgE4UdljPtNvDOL', 'MarisOtter SMASH', 'COMPLETED', 'WAITING', 'KEG');
-INSERT INTO batch (brewfather_id, name, status, tap_status, packaging)              VALUES ('NLltIcoo87foHbTz1N8rH0v0KXht6q', 'Juicy SafAle',     'COMPLETED', 'WAITING', 'KEG');
-INSERT INTO batch (brewfather_id, name, status, tap_status, packaging)              VALUES ('NLltIcoo87foHbTz1N8rH0v0KXht6P', 'Juicy NEIPA',      'COMPLETED', 'WAITING', 'KEG');
-INSERT INTO batch (brewfather_id, name, status, tap_status, packaging)              VALUES ('NLltIcoo87foHbTz1N8rH0v0KXht6A', 'NETTO',            'COMPLETED', 'WAITING', 'KEG');
+INSERT INTO batch (id, brewfather_id, name, status, tap_status, packaging, recipe, tap) VALUES (10, 'LAXI2KWZXcU2pBpzrfg6B3Uy5940vQ', 'Eldon',            'COMPLETED', 'SERVING', 'KEG', 100001, 1);
+INSERT INTO batch (id, brewfather_id, name, status, tap_status, packaging, recipe, tap) VALUES (11, '6TY4Hg2JfQA0zBnscwspPsVF29JAKM', 'KIWI Pilsner',     'COMPLETED', 'SERVING', 'KEG', 100002, 2);
+INSERT INTO batch (id, brewfather_id, name, status, tap_status, packaging)              VALUES (12, 'HY27A73dYWZNMxapgE4UdljPtNvDOL', 'MarisOtter SMASH', 'COMPLETED', 'WAITING', 'KEG');
+INSERT INTO batch (id, brewfather_id, name, status, tap_status, packaging)              VALUES (13, 'NLltIcoo87foHbTz1N8rH0v0KXht6q', 'Juicy SafAle',     'COMPLETED', 'WAITING', 'KEG');
+INSERT INTO batch (id, brewfather_id, name, status, tap_status, packaging)              VALUES (14, 'NLltIcoo87foHbTz1N8rH0v0KXht6P', 'Juicy NEIPA',      'COMPLETED', 'WAITING', 'KEG');
+INSERT INTO batch (id, brewfather_id, name, status, tap_status, packaging)              VALUES (15, 'NLltIcoo87foHbTz1N8rH0v0KXht6A', 'NETTO',            'COMPLETED', 'WAITING', 'KEG');
+
+INSERT INTO tap (id, batch) VALUES (1, 10);
+INSERT INTO tap (id, batch) VALUES (2, 11);
+INSERT INTO tap (id) VALUES (3);
+INSERT INTO tap (id) VALUES (4);
+
