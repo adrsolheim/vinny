50b74c6b1531388eb4832cf415e8b24bcc57c752
Adrian Solheim <adrsolheim@gmail.com>
2023-11-26 13:01:34 +0100

    Buttons trigger refetch per resource

--- lightjs/index.js
+++ lightjs/index.js
@@ -21,19 +21,44 @@ async function fetchData(url, resource) {
     const response = await fetch(request);
     const data = await response.json();
 
-    const ul = document.getElementById("results")
-    const jsonList = JSON.stringify(data);
-    const list = JSON.parse(jsonList);
-    for (let x in list) {
-        let li = document.createElement("li");
-        li.innerHTML = JSON.stringify(list[x]);
-        ul.append(li);
-    }
-
-    return data;
+    return JSON.parse(JSON.stringify(data));
 }
 
-baseUrl = "http://localhost:8080/api"
-fetchData(baseUrl, "/batches").finally(() => console.log("done!"))
 
+async function populateCategories(categories) {
+    const baseUrl = "http://localhost:8080/api/"
+    for (i in categories) {
+        let category = categories[i];
+        let jsonList = await fetchData(baseUrl, category);
+        let div = document.getElementById(category);
+        let ul = document.createElement("ul");
+        ul.setAttribute("id", `${category}Ul`);
+        div.prepend(ul);
+        for (element of jsonList) {
+            let li = document.createElement("li");
+            li.innerHTML = JSON.stringify(element);
+            ul.appendChild(li);
+        }
+    }
+}
 
+function createButtons(category) {
+    let div = document.getElementById(category);
+    let btn = document.createElement("button");
+    btn.innerHTML = "Fetch";
+    btn.setAttribute("id", `${category}Btn`);
+    div.appendChild(btn)
+    btn.addEventListener("click", function() {
+        let ul = document.getElementById(`${category}Ul`);
+        ul.parentElement.removeChild(ul);
+        populateCategories([category]);
+    });
+}
+
+const categories = ["batches", "recipes", "taphouse"];
+populateCategories(categories).then(() => {
+    for (category of categories) {
+        createButtons(category);
+    }
+})
+//fetchData("http://localhost:8080/api","/batches")
