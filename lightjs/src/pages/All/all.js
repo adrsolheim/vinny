483e1a25d5b0ef46e8a4669da70e391c58be28f7
Adrian Solheim <adrsolheim@gmail.com>
2023-12-12 21:00:08 +0100

    Populate table with batches

--- lightjs/src/pages/All/all.js
+++ lightjs/src/pages/All/all.js
@@ -1,1 +1,26 @@
 import { batches } from "/src/services/service.js"
+
+
+
+
+function createTable() {
+    let table = document.getElementById("batches");
+    batches().then(arr => arr.forEach(batch => {
+        console.log(batch)
+        let tr = `
+        <tr>
+            <td>${batch.id}</td>
+            <td>${batch.brewfatherId}</td>
+            <td>${batch.name}</td>
+            <td>${batch.status}</td>
+            <td>${batch.tapStatus}</td>
+            <td>${batch.packaging}</td>
+        </tr>
+        `;
+        table.innerHTML += tr;
+    }
+    ))
+
+}
+
+createTable();
