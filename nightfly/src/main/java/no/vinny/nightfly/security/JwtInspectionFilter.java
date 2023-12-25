b99c39937b6c13739e069fe2bd5921d7ae14a682
Adrian Solheim <adrsolheim@gmail.com>
2023-12-25 16:33:19 +0100

    Inspect parsed JWT

---
+++ nightfly/src/main/java/no/vinny/nightfly/security/JwtInspectionFilter.java
@@ -0,0 +1,36 @@
+package no.vinny.nightfly.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import java.io.IOException;
+
+@Slf4j
+public class JwtInspectionFilter extends BasicAuthenticationFilter {
+
+    public JwtInspectionFilter(AuthenticationManager authenticationManager) {
+        super(authenticationManager);
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication != null) {
+            log.info("Credentials: {}", authentication.getCredentials());
+            log.info("Authorities: {}", authentication.getAuthorities());
+            log.info("Details: {}", authentication.getDetails());
+            log.info("Principal: {}", (SupabaseUser) authentication.getPrincipal());
+        }
+
+        chain.doFilter(request, response);
+    }
+}
