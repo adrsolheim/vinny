ac7f47149dba97401c14026e6e41280afaa770ab
adrsolheim <adrsolheim@gmail.com>
2024-01-06 23:54:08 +0100

    ✨ Database schema add BatchUnit

--- nightfly/src/main/resources/h2init.sql
+++ nightfly/src/main/resources/h2init.sql
@@ -1,28 +1,46 @@
 --DROP DATABASE IF EXISTS nightfly;
 --CREATE DATABASE IF NOT EXISTS nightfly;
 DROP TABLE IF EXISTS tap;
+DROP TABLE IF EXISTS batch_unit;
+DROP TABLE IF EXISTS keg;
 DROP TABLE IF EXISTS batch;
 DROP TABLE IF EXISTS recipe;
 
 DROP TABLE IF EXISTS account;
-DROP TABLE IF EXISTS keg;
 
 CREATE TABLE IF NOT EXISTS recipe (
-    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id BIGINT NOT NULL AUTO_INCREMENT(100) PRIMARY KEY,
     brewfather_id VARCHAR(50) UNIQUE,
     name VARCHAR(100)
 );
 
 CREATE TABLE IF NOT EXISTS batch (
-    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id BIGINT NOT NULL AUTO_INCREMENT(100) PRIMARY KEY,
     brewfather_id VARCHAR(50) UNIQUE,
     name VARCHAR(100),
     status VARCHAR(30),
-    tap_status VARCHAR(30),
-    packaging VARCHAR(20),
     recipe BIGINT,
-    tap TINYINT,
     FOREIGN KEY (recipe) REFERENCES recipe(id)
+);
+
+CREATE TABLE IF NOT EXISTS keg (
+    id BIGINT NOT NULL AUTO_INCREMENT(100) PRIMARY KEY,
+    capacity DECIMAL,
+    brand VARCHAR(100),
+    serial_number VARCHAR(255),
+    purchase_condition VARCHAR(100),
+    note VARCHAR (1000)
+);
+
+CREATE TABLE IF NOT EXISTS batch_unit (
+    id BIGINT NOT NULL AUTO_INCREMENT(100) PRIMARY KEY,
+    batch BIGINT,
+    packaging VARCHAR(30),
+    tap_status VARCHAR(30),
+    volume_status VARCHAR(30),
+    keg BIGINT,
+    FOREIGN KEY (batch) REFERENCES batch(id),
+    FOREIGN KEY (keg) REFERENCES keg(id)
 );
 
 CREATE TABLE IF NOT EXISTS tap (
@@ -32,19 +50,11 @@ CREATE TABLE IF NOT EXISTS tap (
 );
 
 CREATE TABLE IF NOT EXISTS account (
-    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id BIGINT NOT NULL AUTO_INCREMENT(100) PRIMARY KEY,
     username VARCHAR(100),
     password VARCHAR(255)
 );
 
-CREATE TABLE IF NOT EXISTS keg (
-    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    capacity DECIMAL,
-    brand VARCHAR(100),
-    serial_number VARCHAR(255),
-    source VARCHAR(100),
-    note VARCHAR (1000)
-);
 
 INSERT INTO recipe (id, brewfather_id, name) VALUES (100001, 'b1W72a2OuL9xsC1c1OTrgog6JiPP0b', 'Eldon');
 INSERT INTO recipe (id, brewfather_id, name) VALUES (100002, 'oCJlXhR1iIdYgLMwem0V0mmYNBIIia', 'KIWI Pilsner');
@@ -56,6 +66,26 @@ INSERT INTO batch (id, brewfather_id, name, status, tap_status, packaging)      
 INSERT INTO batch (id, brewfather_id, name, status, tap_status, packaging)              VALUES (14, 'NLltIcoo87foHbTz1N8rH0v0KXht6P', 'Juicy NEIPA',      'COMPLETED', 'WAITING', 'KEG');
 INSERT INTO batch (id, brewfather_id, name, status, tap_status, packaging)              VALUES (15, 'NLltIcoo87foHbTz1N8rH0v0KXht6A', 'NETTO',            'COMPLETED', 'WAITING', 'KEG');
 
+INSERT INTO keg (id, capacity, brand, serial_number, purchase_condition, note) VALUES (1,  23,  'AEB',     null, 'STORE', null);
+INSERT INTO keg (id, capacity, brand, serial_number, purchase_condition, note) VALUES (2,  23,  'AEB',     null, 'STORE', null);
+INSERT INTO keg (id, capacity, brand, serial_number, purchase_condition, note) VALUES (3,  19,  'AEB',     null, 'STORE', null);
+INSERT INTO keg (id, capacity, brand, serial_number, purchase_condition, note) VALUES (4,  19,  'AEB',     null, 'STORE', null);
+INSERT INTO keg (id, capacity, brand, serial_number, purchase_condition, note) VALUES (5,  19,  'AEB',     null, 'STORE', null);
+INSERT INTO keg (id, capacity, brand, serial_number, purchase_condition, note) VALUES (6,  19,  'KEGLAND', null, 'STORE', null);
+INSERT INTO keg (id, capacity, brand, serial_number, purchase_condition, note) VALUES (7,  19,  'KEGLAND', null, 'STORE', null);
+INSERT INTO keg (id, capacity, brand, serial_number, purchase_condition, note) VALUES (8,  12,  'KEGLAND', null, 'STORE', null);
+INSERT INTO keg (id, capacity, brand, serial_number, purchase_condition, note) VALUES (9,  9.5, 'KEGLAND', null, 'STORE', null);
+INSERT INTO keg (id, capacity, brand, serial_number, purchase_condition, note) VALUES (10, 9.5, 'UNKNOWN', null, 'USED', 'Fat fra utlandet / brusfat');
+INSERT INTO keg (id, capacity, brand, serial_number, purchase_condition, note) VALUES (11, 9.5, 'UNKNOWN', null, 'USED', 'Fat fra utlandet / brusfat');
+INSERT INTO keg (id, capacity, brand, serial_number, purchase_condition, note) VALUES (12, 9.5, 'UNKNOWN', null, 'USED', 'Fat fra utlandet / brusfat');
+
+INSERT INTO batch_unit (id, batch, tap_status, packaging, volume_status, keg) VALUES (1, 10, 'CONNECTED',    'KEG', 'NOT_EMPTY', 1);
+INSERT INTO batch_unit (id, batch, tap_status, packaging, volume_status, keg) VALUES (2, 10, 'WAITING',      'KEG', 'NOT_EMPTY', 2);
+INSERT INTO batch_unit (id, batch, tap_status, packaging, volume_status, keg) VALUES (3, 11, 'SERVING',      'KEG', 'NOT_EMPTY', 3);
+INSERT INTO batch_unit (id, batch, tap_status, packaging, volume_status, keg) VALUES (4, 11, 'DISCONNECTED', 'KEG', 'EMPTY',     4);
+INSERT INTO batch_unit (id, batch, tap_status, packaging, volume_status, keg) VALUES (5, 12, 'DISCONNECTED', 'KEG', 'EMPTY',     5);
+INSERT INTO batch_unit (id, batch, tap_status, packaging, volume_status, keg) VALUES (6, 12, 'DISCONNECTED', 'KEG', 'NOT_EMPTY', 6);
+
 INSERT INTO tap (id, batch) VALUES (1, 10);
 INSERT INTO tap (id, batch) VALUES (2, 11);
 INSERT INTO tap (id) VALUES (3);
@@ -63,15 +93,3 @@ INSERT INTO tap (id) VALUES (4);
 
 INSERT INTO account (username, password) VALUES ('', '');
 
-INSERT INTO keg (capacity, brand, serial_number, source, note) VALUES (23,  'AEB',     null, 'STORE', null);
-INSERT INTO keg (capacity, brand, serial_number, source, note) VALUES (23,  'AEB',     null, 'STORE', null);
-INSERT INTO keg (capacity, brand, serial_number, source, note) VALUES (19,  'AEB',     null, 'STORE', null);
-INSERT INTO keg (capacity, brand, serial_number, source, note) VALUES (19,  'AEB',     null, 'STORE', null);
-INSERT INTO keg (capacity, brand, serial_number, source, note) VALUES (19,  'AEB',     null, 'STORE', null);
-INSERT INTO keg (capacity, brand, serial_number, source, note) VALUES (19,  'KEGLAND', null, 'STORE', null);
-INSERT INTO keg (capacity, brand, serial_number, source, note) VALUES (19,  'KEGLAND', null, 'STORE', null);
-INSERT INTO keg (capacity, brand, serial_number, source, note) VALUES (12,  'KEGLAND', null, 'STORE', null);
-INSERT INTO keg (capacity, brand, serial_number, source, note) VALUES (9.5, 'KEGLAND', null, 'STORE', null);
-INSERT INTO keg (capacity, brand, serial_number, source, note) VALUES (9.5, 'UNKNOWN', null, 'USED', 'Fat fra utlandet / brusfat');
-INSERT INTO keg (capacity, brand, serial_number, source, note) VALUES (9.5, 'UNKNOWN', null, 'USED', 'Fat fra utlandet / brusfat');
-INSERT INTO keg (capacity, brand, serial_number, source, note) VALUES (9.5, 'UNKNOWN', null, 'USED', 'Fat fra utlandet / brusfat');
--- nightfly_db/scripts/1_init.sql
+++ nightfly_db/scripts/1_init.sql
@@ -1,4 +1,3 @@
-DROP DATABASE IF EXISTS nightfly;
 CREATE DATABASE IF NOT EXISTS nightfly;
 
 CREATE TABLE IF NOT EXISTS nightfly.recipe (
@@ -12,17 +11,8 @@ CREATE TABLE IF NOT EXISTS nightfly.batch (
     brewfather_id VARCHAR(50) UNIQUE,
     name VARCHAR(100),
     status VARCHAR(30),
-    tap_status VARCHAR(30),
-    packaging VARCHAR(20),
     recipe BIGINT,
-    tap TINYINT,
     FOREIGN KEY (recipe) REFERENCES recipe(id)
-);
-
-CREATE TABLE IF NOT EXISTS nightfly.tap (
-    id TINYINT NOT NULL PRIMARY KEY,
-    batch BIGINT,
-    FOREIGN KEY (batch) REFERENCES batch(id)
 );
 
 CREATE TABLE IF NOT EXISTS nightfly.keg (
@@ -30,6 +20,23 @@ CREATE TABLE IF NOT EXISTS nightfly.keg (
     capacity DECIMAL,
     brand VARCHAR(100),
     serial_number VARCHAR(255),
-    source VARCHAR(100),
+    purchase_condition VARCHAR(100),
     note VARCHAR (1000)
+);
+
+CREATE TABLE IF NOT EXISTS nightfly.batch_unit (
+    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    batch BIGINT,
+    packaging VARCHAR(30),
+    tap_status VARCHAR(30),
+    volume_status VARCHAR(30),
+    keg BIGINT,
+    FOREIGN KEY (batch) REFERENCES batch(id),
+    FOREIGN KEY (keg) REFERENCES keg(id)
+);
+
+CREATE TABLE IF NOT EXISTS nightfly.tap (
+    id TINYINT NOT NULL PRIMARY KEY,
+    batch BIGINT,
+    FOREIGN KEY (batch) REFERENCES batch(id)
 );
