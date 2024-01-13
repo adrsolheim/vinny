2988cbde40e6af4566aba38e62e45a1af1a470fd
Adrian Solheim <adrsolheim@gmail.com>
2024-01-13 13:07:21 +0100

    Spring Authorization Server

---
+++ gatekeeper/src/test/java/no/vinny/gatekeeper/GatekeeperApplicationTests.java
@@ -0,0 +1,13 @@
+package no.vinny.gatekeeper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatekeeperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
