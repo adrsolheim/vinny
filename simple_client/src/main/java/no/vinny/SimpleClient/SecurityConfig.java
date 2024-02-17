39f83248e1fac45d5fc2856f597562b91e8907aa
Adrian Solheim <adrsolheim@gmail.com>
2024-02-17 14:35:00 +0100

    🔒️ 🚑️ MAKE SURE TO PUT SECRET IN THE CORRECT CONFIG FIELD !!
    Thank you google for providing a usable error message

---
+++ simple_client/src/main/java/no/vinny/SimpleClient/SecurityConfig.java
@@ -0,0 +1,26 @@
+package no.vinny.SimpleClient;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(authorize -> {
+                    authorize.requestMatchers("/error").permitAll();
+                    authorize.requestMatchers("/favicon.ico").permitAll();
+                    authorize.anyRequest().authenticated();
+                })
+                .csrf(httpSecurityCsrfConfigurer -> httpSecurityCsrfConfigurer.disable())
+                .oauth2Login(Customizer.withDefaults())
+                .build();
+    }
+}
