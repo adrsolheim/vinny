26c7ec48eadd810fa286b5801b8e9a2f8c78c9e2
Adrian Solheim <adrsolheim@gmail.com>
2024-02-21 21:12:13 +0100

    Commit: gatekeeper

--- gatekeeper/src/main/java/no/vinny/gatekeeper/config/SecurityConfig.java
+++ gatekeeper/src/main/java/no/vinny/gatekeeper/config/SecurityConfig.java
@@ -7,7 +7,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configurers.userdetails.DaoAuthenticationConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -18,9 +24,24 @@ import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 
 @Configuration
 public class SecurityConfig {
+
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
+        //return username -> userRepository.findByUsername(username);
+        return new InMemoryUserDetailsManager(
+                User.builder()
+                        .username("user")
+                        .password(passwordEncoder().encode("password"))
+                        .roles("USER")
+                        .build()
+        );
+    }
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
@@ -30,16 +51,20 @@ public class SecurityConfig {
     public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(httpSecurityCsrfConfigurer -> httpSecurityCsrfConfigurer.disable())
+                //.oauth2Login(oauth2Login -> oauth2Login.permitAll())
                 .authorizeHttpRequests(authorize -> authorize
-                                .requestMatchers( "/oauth/token").permitAll()
-                                .requestMatchers( "/logout").permitAll()
-                                .anyRequest().authenticated()
-    //                    .requestMatchers( "/login/**").permitAll()
-    //                    .requestMatchers( "/authorize").permitAll()
-    //                    .requestMauchers( "/authorize/**").permitAll()
-    //                    .anyRequest().authenticated())
-    //            .formLogin(Customizer.withDefaults())
+                        .requestMatchers( "/oauth2/**").permitAll()
+                        .requestMatchers( "/login/**").permitAll()
+                        .requestMatchers( "/authorize/**").permitAll()
+                        .anyRequest().authenticated()
                 )
+                .formLogin(Customizer.withDefaults())
                 .build();
     }
+
+    @Bean
+    public JwtDecoder jwtDecoder(JWKSource<SecurityContext> jwkSource) {
+        return OAuth2AuthorizationServerConfiguration.jwtDecoder(jwkSource);
+    }
+
 }
