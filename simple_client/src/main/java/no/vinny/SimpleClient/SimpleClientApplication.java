18f6b35b8a5b7ffcf3cabaeb7b2ac804dc61b86b
Adrian Solheim <adrsolheim@gmail.com>
2024-02-22 21:03:00 +0100

    Attempt to lock down endpoints by roles

--- simple_client/src/main/java/no/vinny/SimpleClient/SimpleClientApplication.java
+++ simple_client/src/main/java/no/vinny/SimpleClient/SimpleClientApplication.java
@@ -2,6 +2,8 @@ package no.vinny.SimpleClient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -18,19 +20,21 @@ public class SimpleClientApplication {
 		SpringApplication.run(SimpleClientApplication.class, args);
 	}
 
-	@GetMapping("/hello")
-	public Map<String, String> root(Principal principal) {
-		return Map.of("greeting", "Hello World!", "principal", principal.getName());
+	@GetMapping("/")
+	public Map<String, String> root(Authentication authentication) {
+		return Map.of("greeting", "Hello World!", "authentication", authentication.toString());
 	}
 
 
+	@PreAuthorize("isAuthenticated() and hasRole('user')")
 	@GetMapping("/batches")
-	public Map<String, String> batches(Principal principal) {
-		return Map.of("b1", "Batch One", "principal", principal.getName());
+	public Map<String, String> batches(Authentication authentication) {
+		return Map.of("b1", "Batch One", "authentication", authentication.toString());
 	}
 
+	@PreAuthorize("isAuthenticated() and hasRole('admin')")
 	@GetMapping("/recipes")
-	public Map<String, String> recipes(Principal principal) {
-		return Map.of("r1", "Recipe One", "principal", principal.getName());
+	public Map<String, String> recipes(Authentication authentication) {
+		return Map.of("r1", "Recipe One", "authentication", authentication.toString());
 	}
 }
