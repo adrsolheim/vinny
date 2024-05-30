8a5ac537658ceb22cdd25243ff2f5b75f661326d
Adrian Solheim <adrsolheim@gmail.com>
2024-05-30 18:31:50 +0200

    Configure dependencies for oauth2

--- nightfly/src/main/java/no/vinny/nightfly/security/SupabaseAuthService.java
+++ nightfly/src/main/java/no/vinny/nightfly/security/SupabaseAuthService.java
@@ -3,7 +3,6 @@ package no.vinny.nightfly.security;
 import com.nimbusds.jose.JOSEException;
 import io.jsonwebtoken.*;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.UnsupportedEncodingException;
