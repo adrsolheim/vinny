81f7a67f0e618350cb9e87a783eccb85c5f9a409
Adrian Solheim <adrsolheim@gmail.com>
2024-06-11 20:58:06 +0200

    Add JdbcTemplate bean

---
+++ gatekeeper/src/main/java/no/vinny/gatekeeper/config/DatabaseConfig.java
@@ -0,0 +1,39 @@
+package no.vinny.gatekeeper.config;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+
+@Slf4j
+@Configuration
+@EnableTransactionManagement
+public class DatabaseConfig {
+
+    @Autowired
+    private Environment env;
+
+
+    @Bean
+    public DataSource h2DataSource() {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(env.getProperty("spring.datasource.jdbc-url"));
+        config.setUsername(env.getProperty("spring.datasource.username"));
+        config.setPassword(env.getProperty("spring.datasource.password"));
+        config.setMaximumPoolSize(10);
+        config.setMinimumIdle(5);
+        return new HikariDataSource(config);
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
+}
