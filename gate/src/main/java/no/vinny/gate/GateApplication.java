bf0d019e8507f667bca5cfd5cef4c8ed0b004345
Adrian Solheim <adrsolheim@gmail.com>
2024-08-10 12:49:44 +0200

    Define gateway routes to frontend and backend

--- gate/src/main/java/no/vinny/gate/GateApplication.java
+++ gate/src/main/java/no/vinny/gate/GateApplication.java
@@ -2,14 +2,6 @@ package no.vinny.gate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.function.RouterFunction;
-import org.springframework.web.servlet.function.ServerResponse;
-
-import static org.springframework.cloud.gateway.server.mvc.filter.TokenRelayFilterFunctions.tokenRelay;
-import static org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions.route;
-import static org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions.http;
-import static org.springframework.cloud.gateway.server.mvc.predicate.GatewayRequestPredicates.path;
 
 @SpringBootApplication
 public class GateApplication {
@@ -17,25 +9,6 @@ public class GateApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GateApplication.class, args);
 	}
-
-	private static final String API_PREFIX = "/api/**";
-	private static final String API_HOST = "http://nightfly:8080";
-
-	private static final String UI_PREFIX = "/**";
-	private static final String UI_HOST = "http://sunflower:9090";
-
-	@Bean
-	public RouterFunction<ServerResponse> apiGet() {
-		return route()
-				.GET(API_PREFIX,  request -> ServerResponse.ok().body("Hello World"))
-				.POST(API_PREFIX, http())
-				.PUT(API_PREFIX, http())
-				.PATCH(API_PREFIX, http())
-				.DELETE(API_PREFIX, http())
-				//.filter(tokenRelay())
-				.build();
-	}
-
 
 }
 
---
+++ gate/src/main/java/no/vinny/gate/route/Routes.java
@@ -0,0 +1,34 @@
+package no.vinny.gate.route;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.ServerResponse;
+
+import static org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions.route;
+import static org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions.http;
+import static org.springframework.cloud.gateway.server.mvc.predicate.GatewayRequestPredicates.path;
+
+@Configuration
+public class Routes {
+
+    private static final String API_PREFIX = "/api/**";
+    private static final String API_HOST = "http://nightfly:8080";
+
+    private static final String UI_PREFIX = "/**";
+    private static final String UI_HOST = "http://sunflower:9090";
+
+    @Bean
+    public RouterFunction<ServerResponse> backendRoute() {
+        return route()
+                .route(path(API_PREFIX),  http(API_HOST))
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> frontendRoute() {
+        return route()
+                .route(path(UI_PREFIX),  http(UI_HOST))
+                .build();
+    }
+}
