a114f2aea7d66505edc6b68cf64f3850cc192be9
Adrian Solheim <adrsolheim@gmail.com>
2024-09-08 15:53:27 +0200

    Add spring_session_attributes table

--- gatekeeper/src/main/resources/h2init.sql
+++ gatekeeper/src/main/resources/h2init.sql
@@ -99,6 +99,16 @@ create unique index if not exists spring_session_ix1 on spring_session using btr
 create index if not exists spring_session_ix2 on spring_session using btree (expiry_time);
 create index if not exists spring_session_ix3 on spring_session using btree (principal_name);
 
+create table if not exists spring_session_attributes
+(
+    session_primary_id character(36)          not null,
+    attribute_name     character varying(200) not null,
+    attribute_bytes    bytea                  not null,
+    primary key (session_primary_id, attribute_name),
+    foreign key (session_primary_id) references spring_session (primary_id)
+        match simple on update no action on delete cascade
+);
+
 create table if not exists rsa_key_pairs
 (
     id          varchar(1000) not null primary key,
