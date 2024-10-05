859a1fc05f96d6b3dcb5dae5a5b6d21579447572
adrsolheim <adrsolheim@gmail.com>
2024-10-05 14:30:47 +0200

    Add gateway to install script

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 # docker compose local build
 rm -rf ./nightfly/target
 rm -rf ./gatekeeper/target
+rm -rf ./gate/target
 
 mvn -f ./nightfly/pom.xml clean install
 mvn -f ./gatekeeper/pom.xml clean install
+mvn -f ./gate/pom.xml clean install
