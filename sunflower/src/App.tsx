e4ce23796299d4e5dc71215accd978f61f624582
adrsolheim <adrsolheim@gmail.com>
2024-12-09 20:46:23 +0100

    Get recipes without auth

--- sunflower/src/App.tsx
+++ sunflower/src/App.tsx
@@ -1,10 +1,26 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [recipes, setRecipes] = useState<Recipe[]>([]);
+  const BASE_URL = 'http://127.0.0.1:8080';
+
+  useEffect(() => {
+    const fetchRecipes = async () => {
+      const response = await fetch(`${BASE_URL}/api/recipes`);
+      const recipes = (await response.json()) as Recipe[];
+      setRecipes(recipes);
+    };
+    fetchRecipes();
+  }, []);
+
+  interface Recipe {
+    id: number,
+    brewfatherId: string,
+    name: string
+  }
 
   return (
     <>
@@ -18,12 +34,11 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <ul>
+          {recipes.map((recipe) => {
+            return <li key={recipe.id}>{recipe.name}</li>
+          })}
+        </ul>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
