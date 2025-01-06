f7ae97da9767fc8553df4104b3d1f25dc7c38a23
adrsolheim <adrsolheim@gmail.com>
2025-01-06 20:33:22 +0100

    Error page automatically redirects back

--- sunflower/src/pages/ErrorPage.tsx
+++ sunflower/src/pages/ErrorPage.tsx
@@ -1,10 +1,19 @@
-import { Link } from "react-router-dom";
+import { useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function NotFound() {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        setTimeout(() => {
+            navigate('/');
+        }, 1500);
+    });
     return (
     <div className="flex flex-col gap-2">
-        404 Not Found
-
+        <h3>404 Not Found</h3>
+        <br />
+        <h4>Redirecting back..</h4>
         <Link to="/">Back to home</Link>
     </div>
     );
