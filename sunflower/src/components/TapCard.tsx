0ca3b94983579878e8e70cd83d48093ab6b8cde2
Adrian Solheim <adrsolheim@gmail.com>
2025-01-22 17:48:38 +0100

    Refactor

--- sunflower/src/components/TapCard.tsx
+++ sunflower/src/components/TapCard.tsx
@@ -2,15 +2,20 @@
 import logo from '../assets/tap_handle.jpg';
 import styles from '../app.module.css';
 import Tap from '../types/tap';
+import { BaselineAddCircleOutline } from '../assets/BaselineAddCircleOutline';
 
 export default function RecipeCard(props: Readonly<{ tap: Tap; }>) {
     const tap: Tap = props.tap;
     return (
         <div className={`${styles.card} ${tap.active ? styles.cardhighlight : ''}`}>
-            <img className={styles.cardimage} src={logo} alt="tap handle logo" />
-            <h2 className={`${styles.cardtitle} ${tap.active ? styles.cardtexthighlight : ''}`}>{tap.batch?.name ?? " "}</h2>
-            <h4 className={styles.cardnumber}>{tap.id}</h4>
-            <input type="button" className={styles.btn} value="+"/>
+            <div className={styles.cardcontent}>
+                <img className={styles.cardimage} src={logo} alt="tap handle logo" />
+                <h2 className={`${styles.cardtitle} ${tap.active ? styles.cardtexthighlight : ''}`}>{tap.batch?.name ?? " "}</h2>
+                <h4 className={styles.cardnumber}>{tap.id}</h4>
+            </div>
+            <div className={styles.cardtail }>
+                <BaselineAddCircleOutline color='white'/>
+            </div>
         </div>
     );
 }
