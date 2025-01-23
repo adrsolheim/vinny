9a742ac08bd4c327a48eb045423ca548b0f7338f
adrsolheim <adrsolheim@gmail.com>
2025-01-23 20:48:32 +0100

    Tweak some styling to the layout

--- sunflower/src/components/TapCard.tsx
+++ sunflower/src/components/TapCard.tsx
@@ -4,13 +4,13 @@ import styles from '../app.module.css';
 import Tap from '../types/tap';
 import { BaselineAddCircleOutline } from '../assets/BaselineAddCircleOutline';
 
-export default function RecipeCard(props: Readonly<{ tap: Tap; }>) {
+export default function TapCard(props: Readonly<{ tap: Tap; }>) {
     const tap: Tap = props.tap;
     return (
         <div className={`${styles.card} ${tap.active ? styles.cardhighlight : ''}`}>
-            <div><img className={styles.cardimage} src={logo} alt="tap handle logo" /></div>
-            <div><h3 className={`${styles.cardtitle} ${tap.active ? styles.cardtexthighlight : ''}`}>{tap.batch?.name ?? " "}</h3></div>
-            <div><h4 className={styles.cardnumber}>{tap.id}</h4></div>
+            <div ><img className={styles.cardimage} src={logo} alt="tap handle logo" /></div>
+            <div className={`${styles.cardtitle} ${tap.active ? styles.cardtexthighlight : ''}`}><p>{tap.batch?.name ?? " "}</p></div>
+            <div className={styles.cardnumber}><p>{tap.id}</p></div>
             <div className={styles.cardtail }>
                 <BaselineAddCircleOutline color='white'/>
             </div>
