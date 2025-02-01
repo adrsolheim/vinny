cf905b8b422a6f7a1c4db77852edd2415feb3e57
adrsolheim <adrsolheim@gmail.com>
2025-02-01 19:59:58 +0100

    Click logo navigates to homepage

--- sunflower/src/components/Header.tsx
+++ sunflower/src/components/Header.tsx
@@ -1,11 +1,12 @@
 import logo from '../assets/orange_beer_mug.svg';
 import styles from '../app.module.css'
+import { useNavigate } from 'react-router-dom';
 
 export default function Header({ sidebarActive, setSidebarActive } : { sidebarActive: boolean, setSidebarActive : Function}) {
-  const clickToggle = () => { setSidebarActive(!sidebarActive) };
   const activate = () => { setSidebarActive(true) };
+  let navigate = useNavigate();
   const deactivate = () => { setSidebarActive(false) };//setTimeout(() => setSidebarActive(false), 500) };
   return (
-    <header><div className={styles.headerlogo} onMouseLeave={deactivate}><img src={logo} alt="beer mug logo" onClick={clickToggle} onMouseOver={activate}/></div></header>
+    <header><div className={styles.headerlogo} onMouseLeave={deactivate}><img src={logo} alt="beer mug logo" onClick={() => navigate("/")} onMouseOver={activate}/></div></header>
   );
 }
--- sunflower/src/components/Sidebar.tsx
+++ sunflower/src/components/Sidebar.tsx
@@ -4,8 +4,9 @@ import styles from '../app.module.css';
 export default function Sidebar({ active, setActive }: {active: boolean, setActive: Function }) {
     const menuItems: string[] = ['Batches', 'Recipes', 'Profile'];
     const activate = () => setActive(true);
+    const deactivate = () => setActive(false);
     return (
-        <div className={ active ? `${styles.sidebar} ${styles.active}` : styles.sidebar} onMouseOver={activate}>
+        <div className={ active ? `${styles.sidebar} ${styles.active}` : styles.sidebar} onMouseOver={activate} onMouseLeave={deactivate}>
             <ul>
                 {menuItems.map((item, idx) => <NavLink key={idx} to={item.toLowerCase()}><li>{item}</li></NavLink>)}
             </ul>
