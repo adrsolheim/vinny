86da9dd1388fdc2fcd5ac91e0f9903f379c25d0d
Adrian Solheim <adrsolheim@gmail.com>
2025-12-30 11:12:17 +0100

    Add bearer token option in swagger

---
+++ nightfly/nightfly-api/src/main/java/no/vinny/nightfly/config/SwaggerConfig.java
@@ -0,0 +1,28 @@
+package no.vinny.nightfly.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        String securityScheme = "Bearer Token";
+        return new OpenAPI()
+                .info(new Info().title("Nightfly API").version("v1"))
+                .addSecurityItem(new SecurityRequirement().addList(securityScheme))
+                .components(new Components()
+                        .addSecuritySchemes(securityScheme, new SecurityScheme()
+                                .name(securityScheme)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                ));
+    }
+}
